b7259f904dd9f0dc57b8cd67c6d03a1798803d84
pallavisingleinterface <50008641+pallavisingleinterface@users.noreply.github.com>
2024-02-11 20:28:02 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,46 @@
+// import Head from "next/head";
+// import Image from "next/image";
+// import { Inter } from "next/font/google";
+// import styles from "@/styles/Home.module.css";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+import HeroBanner from "@/components/HeroBanner";
+import ProductCard from "@/components/ProductCard";
+import Wrapper from "@/components/Wrapper";
+
+export default function Home() {
+  return (
+      <main>
+        <HeroBanner />
+        <Wrapper>
+           {/* heading and paragaph start */}
+
+           <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+                <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+                    GIVE THE PERFECT GIFT
+                </div>
+                <div className="text-md md:text-xl">
+                    Find the perfect pair for you and yours this valentine's Day
+                </div>
+            </div>
+
+            {/* heading and paragaph end */}
+
+            {/* products grid start */}
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+            </div>   
+
+            {/* products grid end */}
+        </Wrapper>
+      </main>
+  );
+}
